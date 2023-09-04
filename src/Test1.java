91c014c8047ed09e22ff04f43405f7a39c09fde0
swagatika <j.swagatika2900@gmail.com>
2023-09-04 11:14:01 -0500

    First Commit.

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+
+	public static void main(String[] args)
+	{
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("Eaty Something");
+		}
+		else
+		{
+			System.out.println("Do Work");
+		}
+	}
+	
+}
+
